d16fab98caa93487f381dfcf75b101a2bff6bf24
VladXII <PobereznichenkoVladimir@gmail.com>
2018-04-11 14:02:47 +0300

    Initial

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "Hello world, It's me";
+echo "Hello world, It's me\n";
+print "I am back";
+// Одностроковий коммент
+// echo "x eguals $x";
+$x += 10; //збільшення значення х на 10
+/* багатостроковий
+   комментар */
+$mycounter = 1;
+$mystring = "Hp 4740s";
+$myarray = ["one", "two", "three"];
+$username = "Vlad";
+echo "<br/>$username";
