930af26dd414b19986ed3094cbd2f7f1cf811f96
VladXII <PobereznichenkoVladimir@gmail.com>
2018-04-15 11:20:19 +0300

    Initial

--- ex2.php
+++ ex2.php
@@ -24,5 +24,9 @@ echo test();
 echo test();
 $came_from = $_SERVER['HTTP_REFERRER'];
 $came_from = htmlentities($_SERVER['HTTP_REFERRER']);
-
+echo "<br>";
+echo "a: [" . (20 > 9) . "]<br> ";
+echo "b: [" . (5 == 6) . "]<br> ";
+echo "c: [" . (1 == 0) . "]<br> ";
+echo "d: [" . (1 == 1) . "]<br> ";
 
