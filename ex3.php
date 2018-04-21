514f314a286e9035c411dcd98bcf19bd75b7476b
VladXII <PobereznichenkoVladimir@gmail.com>
2018-04-21 16:33:55 +0300

    Initial

--- ex3.php
+++ ex3.php
@@ -7,7 +7,9 @@
  */
 print(ucfirst(strtolower("i wAS aN engInERr aNd nOW I leArN pHP\n ")));
 print ucfirst(strtolower("coMe tO mE\n"));
+
 echo fix_names("tERRens", "claUS", "mOOdy");
+
 function fix_names($n1, $n2 ,$n3)
 {
     $n1 = ucfirst(strtolower($n1));
@@ -16,12 +18,29 @@ function fix_names($n1, $n2 ,$n3)
     return  " $n1 " . "$n2 " . "$n3 ";
 
 }
+
 $names_2 = fix_names_2("boBBy", "cAnE", "mARtin");
-echo $names_2[0] . " " . $names_2[1] . " " . $names_2[2];
+echo $names_2[0] . " " . $names_2[1] . " " . $names_2[2] . "<br>";
+
 function fix_names_2($n1, $n2 ,$n3)
 {
 $n1 = ucfirst(strtolower($n1));
 $n2 = ucfirst(strtolower($n2));
 $n3 = ucfirst(strtolower($n3));
 return array($n1, $n2, $n3);
+}
+
+$b1 = "vlAD";
+$b2 = "vLaD";
+$b3 = "PoberZniChENko";
+
+echo $b1 . " " . $b2 . " " . $b3 . "<br>";
+fix_names_3($b1, $b2, $b3);
+echo $b1 . " " . $b2 . " " .$b3;
+
+function fix_names_3(&$n1, &$n2 , &$n3)
+{
+  $n1 = ucfirst(strtolower($n1));
+  $n2 = ucfirst(strtolower($n2));
+  $n3 = ucfirst(strtolower($n3));
 }
